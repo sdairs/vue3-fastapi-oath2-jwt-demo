a71fd7c7dab295b864e18ce5059849acd8d26817
sdairs <me@alasdairb.com>
2021-12-18 14:11:52 +0000

    Update SQL init commands to work

--- database/init.sql
+++ database/init.sql
@@ -1,4 +1,6 @@
+# CockroachDB
 CREATE ROLE useradmin WITH LOGIN;
-CREATE DATABASE users OWNER useradmin;
-CREATE TABLE users.users (username STRING PRIMARY KEY, password STRING NOT NULL) OWNER useradmin;
+CREATE DATABASE users;
+CREATE TABLE users.users (username STRING PRIMARY KEY, password STRING NOT NULL);
 INSERT INTO users.users VALUES ('user1','$argon2id$v=19$m=102400,t=2,p=8$HgNACAGA8F7LGQPgXKu1lg$A6+Cgwfax9j8JJc/dc/yFA');
+
